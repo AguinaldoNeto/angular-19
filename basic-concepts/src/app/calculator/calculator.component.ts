b28308ca799320e897e5ccfae64183c7418df344
aguinaldo-neto <neto_aguinaldo@outloook.com>
2025-05-22 20:26:01 -0300

    3 -  adds logic into the calculator component

--- basic-concepts/src/app/calculator/calculator.component.ts
+++ basic-concepts/src/app/calculator/calculator.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+
+  numberOne: number = 0;
+  numberTwo: number = 0;
+  result: number = 0;
+
+  calculateResult() {
+    this.result = this.numberOne + this.numberTwo;
+  }
 
 }
